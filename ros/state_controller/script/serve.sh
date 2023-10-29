8b92523bfbf2aae8d4466de88d7a0838ed3f62e8
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-10-29 13:44:59 -0700

    fix: remove poetry reference from ros package

--- ros/state_controller/script/serve.sh
+++ ros/state_controller/script/serve.sh
@@ -4,7 +4,5 @@ set -ex
 # cd to the directory of this script
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# TODO: poetry isn't playing nicely and installing to the system, so we need
-# to call this via poetry for now.
 # https://fastapi.tiangolo.com/tutorial/first-steps/
-poetry run uvicorn app:app --reload --port 8000 --host 0.0.0.0
+uvicorn app:app --reload --port 8000 --host 0.0.0.0
