c7f0356b9f3196e2b254fd5cabe17d27fb4ffd5b
cerashdan <abdullatif.rashdan@gmail.com>
2023-10-30 21:10:24 +0300

    receving image succesfully

--- ros/img2vec/scripts/img2vec.py
+++ ros/img2vec/scripts/img2vec.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 import sys
 import rospy
 from sensor_msgs.msg import Image
@@ -10,10 +9,10 @@ import cv2
 class image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.image_pub = rospy.Publisher("image_topic_2",Image, queue_size=10)
 
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("camera_topic",Image,self.callback)
+    self.image_sub = rospy.Subscriber("image_raw",Image,self.callback)
 
   def callback(self,data):
     try:
@@ -24,7 +23,11 @@ class image_converter:
     (rows,cols,channels) = cv_image.shape
     if cols > 60 and rows > 60 :
       cv2.circle(cv_image, (50,50), 10, 255)
-
+    print("image received ")
+    print(type(cv_image))
+    print(rows)
+    print(cols)
+    
     cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
 
@@ -37,6 +40,7 @@ class image_converter:
 
 def main():
   ic = image_converter()
+  print("initializing img2vec")
   rospy.init_node('image_converter', anonymous=True)
   try:
     rospy.spin()
@@ -46,27 +50,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-import rospy
-from std_msgs.msg import String
-
-
-
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    pub.publish("test succeeded with: " + data.data)
-def listener():
-    rospy.init_node('img2vec', anonymous=True)
-    rospy.Subscriber("cemra_topic", String, callback)
-    global pub
-    pub = rospy.Publisher('img_embeddings', String, queue_size=10)
-
-    rospy.spin()
-
-
-if __name__ == '__main__':
-    try:
-        listener()
-    except rospy.ROSInterruptException:
-        pass
->>>>>>> 141ef0f806a77ac899520bd920e20c4505ead37c
